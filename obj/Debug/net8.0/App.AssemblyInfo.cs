59b70b2f23ea670671f1a07597a8365d500b6e38
sinhhhhhhh <sinhnodejs@gmail.com>
2025-03-01 21:26:28 +0700

    add healcheck

--- obj/Debug/net8.0/App.AssemblyInfo.cs
+++ obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5409b8f3d4abd9df0b6679d1d39842f939657cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c017521852fa7d6a81b3aff01ee7e3a58a7270")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
